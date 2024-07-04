024388ca16f9139faf72fc7ebb20a020bf27c3f5
lenovo <lenovo@DESKTOP-U9KQCEB>
2024-07-04 08:13:24 -0700

    my first commit

---
+++ src/CodePackage/test.java
@@ -0,0 +1,25 @@
+package CodePackage;
+
+
+class Studentt{
+	int i;
+	String name;
+	
+	void insert(int a,String b) {
+		i = a;
+		name = b;
+	}
+	void display() {
+		System.out.println(i + name);
+	}
+}
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Studentt s1 = new Studentt();
+        s1.insert(34, "Ajay");
+        s1.display();
+	}
+
+}
